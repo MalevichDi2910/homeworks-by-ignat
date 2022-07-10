8a963de3689a4809b6de1ec147cb6eda9fb0b02e
Maldi2910 <malevich_diana15@mail.ru>
2022-07-10 22:31:46 +0300

    +[HI] - added project and init github pages

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Message from './Message'
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const messageData = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Diana Malevich',
+    message: 'Hello, how are you?',
+    time: '23:32',
+}
+
+function HW1() {
+    return (
+        <div>
+            <hr/>
+            homeworks 1
+
+            {/*should work (должно работать)*/}
+
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW1
